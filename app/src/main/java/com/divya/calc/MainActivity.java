2d8a59ae8c1ab04719bb2f08285e100009d8a5ac
Divya Gupta <divygupta0319@gmail.com>
2016-10-18 19:00:07 +0530

    [ADD] Second Commit

---
+++ app/src/main/java/com/divya/calc/MainActivity.java
@@ -0,0 +1,23 @@
+package com.divya.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+	Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,clear,clrscr,left_b,right_b;
+	Button minus,plus,multiply,divide,equals;
+	EditText edittext1;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	
+	}
+}
